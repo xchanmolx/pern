d201d5029217082958c0f7493e16f910240f5fdf
xchanmolx <cmanigos@gmail.com>
2019-12-21 18:49:44 +0800

    abtik-pc first commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 var db = require('./database');
 
 const ENV = process.env.NODE_ENV;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 const app = express();
 
