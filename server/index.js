0fd320a3e53576bb4e22e2cebe93dcb28f0c5971
xchanmolx <cmanigos@gmail.com>
2019-12-21 18:58:36 +0800

    commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const server = http.createServer(app);
+const server = http.createServer(app); 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
