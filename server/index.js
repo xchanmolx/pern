3ff91407d0940814e1409c48f91bbe31ed0bdce0
xchanmolx <cmanigos@gmail.com>
2019-12-21 18:36:23 +0800

    weather

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false})); 
+app.use(express.urlencoded({extended: false}));  
 app.use(bodyParser.json());
 
 app.use('/api/cities', require('./api/cities'));
